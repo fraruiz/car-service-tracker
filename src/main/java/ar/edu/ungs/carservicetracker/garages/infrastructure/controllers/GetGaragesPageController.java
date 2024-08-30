d4669874d57ea82da6bed03cd359f1730b06e34a
fraruiz <franciscoruizlezcano@gmail.com>
2024-08-30 18:39:35 -0300

    feat: inject values into templates

--- src/main/java/ar/edu/ungs/carservicetracker/garages/infrastructure/controllers/GetGaragesPageController.java
+++ src/main/java/ar/edu/ungs/carservicetracker/garages/infrastructure/controllers/GetGaragesPageController.java
@@ -1,12 +1,24 @@
 package ar.edu.ungs.carservicetracker.garages.infrastructure.controllers;
 
+import ar.edu.ungs.carservicetracker.garages.application.search.GaragesSearcher;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GetGaragesPageController {
+    private final GaragesSearcher garagesSearcher;
+
+    public GetGaragesPageController(GaragesSearcher garagesSearcher) {
+        this.garagesSearcher = garagesSearcher;
+    }
+
     @GetMapping("/garages")
-    public String handle() {
+    public String handle(Model model) {
+        var values = garagesSearcher.execute();
+
+        model.addAttribute("values", values);
+
         return "garages/index";
     }
 }
--- src/main/java/ar/edu/ungs/carservicetracker/vehicles/infrastructure/controllers/GetVehiclesPageController.java
+++ src/main/java/ar/edu/ungs/carservicetracker/vehicles/infrastructure/controllers/GetVehiclesPageController.java
@@ -1,12 +1,27 @@
 package ar.edu.ungs.carservicetracker.vehicles.infrastructure.controllers;
 
+import ar.edu.ungs.carservicetracker.vehicles.application.VehicleResponse;
+import ar.edu.ungs.carservicetracker.vehicles.application.search.VehicleSearcher;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class GetVehiclesPageController {
+    private final VehicleSearcher vehicleSearcher;
+
+    public GetVehiclesPageController(VehicleSearcher vehicleSearcher) {
+        this.vehicleSearcher = vehicleSearcher;
+    }
+
     @GetMapping("/vehicles")
-    public String handle() {
+    public String handle(Model model) {
+        List<VehicleResponse> responses = this.vehicleSearcher.execute();
+
+        model.addAttribute("values", responses);
+
         return "vehicles/index";
     }
 }

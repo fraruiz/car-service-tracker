cf94eb8471c304e1380c097ae39ba76790e29168
fraruiz <franciscoruizlezcano@gmail.com>
2024-09-07 22:02:17 -0300

    enhancement(services): add link to update in table

--- src/main/java/ar/edu/ungs/carservicetracker/services/infrastructure/controllers/update/GetUpdateServicePageController.java
+++ src/main/java/ar/edu/ungs/carservicetracker/services/infrastructure/controllers/update/GetUpdateServicePageController.java
@@ -2,15 +2,20 @@ package ar.edu.ungs.carservicetracker.services.infrastructure.controllers.update
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GetUpdateServicePageController {
-    @GetMapping("/services/update")
-    public String handle(HttpSession session) {
+    @GetMapping("/services/update/{id}")
+    public String handle(HttpSession session, @PathVariable String id, Model model) {
         if (session.getAttribute("user_id") == null) {
             return "redirect:/auth";
         }
+
+        model.addAttribute("id", id);
 
         return "services/update/index";
     }
